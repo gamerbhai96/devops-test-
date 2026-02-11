d8e27e47f22e8ce8fd47325cee36740c0ef9ce3e
gamerbhai96 <9343257976ab@gmail.com>
2026-02-11 12:32:48 +0530

    add

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@ git clone https://github.com/gamerbhai96/devops-test-.git app || (cd app && git 
 
 # Build and run the Docker container
 cd app
-docker build -t my-app .
-docker stop my-app-container || true
-docker rm my-app-container || true
-docker run -d --name my-app-container -p 80:80 my-app
+sudo docker build -t my-app .
+sudo docker stop my-app-container || true
+sudo docker rm my-app-container || true
+sudo docker run -d --name my-app-container -p 80:80 my-app
 
 echo "Deployment completed successfully!"
